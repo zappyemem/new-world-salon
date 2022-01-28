6af04974f5be32191f6c57fa556606222bd5ba45
Emem Simon <zappy4chat@yahoo.com>
2022-01-28 13:34:14 +0000

    added logic to remove and add products

--- cart/contexts.py
+++ cart/contexts.py
@@ -11,7 +11,7 @@ def cart_contents(request):
     cart = request.session.get('cart', {})
 
     for item_id, item_data in cart.items():
-        for isinstance(item_data, int):
+        if isinstance(item_data, int):
             product = get_object_or_404(Product, pk=item_id)
             total += item_data * product.price
             product_count += item_data
@@ -28,7 +28,7 @@ def cart_contents(request):
                 product_count += quantity
                 cart_items.append({
                     'item_id': item_id,
-                    'quantity': item_data,
+                    'quantity': quantity,
                     'product': product,
                     'size': size,
                 })
