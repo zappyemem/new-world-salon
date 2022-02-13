7d549337e8c88ab43ddcaf076fe6e559e9194218
Emem Simon <zappy4chat@yahoo.com>
2022-02-13 13:03:29 +0000

    replaced scret key and set debug

--- new_world_store/settings.py
+++ new_world_store/settings.py
@@ -21,10 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-m7n*$%b-z0xezcqdsrxcdc441bl8j-ipvio4c#m!z6%r4wq3u%'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['new-world-salon.herokuapp.com', 'localhost']
 
